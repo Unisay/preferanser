a759a860c0dc1eb454faa962ac664ba454d695e2
Yuriy Lazarev <ylazarev@groupon.com>
2013-10-08 18:18:11 +0200

    Cards added

--- src/main/java/com/preferanser/client/application/table/TableView.java
+++ src/main/java/com/preferanser/client/application/table/TableView.java
@@ -21,10 +21,15 @@ package com.preferanser.client.application.table;
 
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.ViewWithUiHandlers;
 import com.preferanser.client.application.PreferanserResources;
+import com.preferanser.server.business.Card;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Table view
@@ -33,13 +38,79 @@ public class TableView extends ViewWithUiHandlers<TableUiHandlers> implements Ta
 
     public interface Binder extends UiBinder<Widget, TableView> {}
 
-    @UiField(provided = true)
-    final PreferanserResources resources;
+    @UiField(provided = true) final PreferanserResources resources;
+
+    private Map<Card, Image> cards = new HashMap<Card, Image>(32);
 
     @Inject
     public TableView(Binder uiBinder, PreferanserResources resources) {
         this.resources = resources;
         initWidget(uiBinder.createAndBindUi(this));
+
+        cards.put(Card.CLUB_SEVEN, c7);
+        cards.put(Card.SPADE_SEVEN, s7);
+        cards.put(Card.DIAMOND_SEVEN, d7);
+        cards.put(Card.HEART_SEVEN, h7);
+        cards.put(Card.CLUB_EIGHT, c8);
+        cards.put(Card.SPADE_EIGHT, s8);
+        cards.put(Card.DIAMOND_EIGHT, d8);
+        cards.put(Card.HEART_EIGHT, h8);
+        cards.put(Card.CLUB_NINE, c9);
+        cards.put(Card.SPADE_NINE, s9);
+        cards.put(Card.DIAMOND_NINE, d9);
+        cards.put(Card.HEART_NINE, h9);
+        cards.put(Card.CLUB_TEN, c10);
+        cards.put(Card.SPADE_TEN, s10);
+        cards.put(Card.DIAMOND_TEN, d10);
+        cards.put(Card.HEART_TEN, h10);
+        cards.put(Card.CLUB_JACK, cj);
+        cards.put(Card.SPADE_JACK, sj);
+        cards.put(Card.DIAMOND_JACK, dj);
+        cards.put(Card.HEART_JACK, hj);
+        cards.put(Card.CLUB_QUEEN, cq);
+        cards.put(Card.SPADE_QUEEN, sq);
+        cards.put(Card.DIAMOND_QUEEN, dq);
+        cards.put(Card.HEART_QUEEN, hq);
+        cards.put(Card.CLUB_KING, ck);
+        cards.put(Card.SPADE_KING, sk);
+        cards.put(Card.DIAMOND_KING, dk);
+        cards.put(Card.HEART_KING, hk);
+        cards.put(Card.CLUB_ACE, ca);
+        cards.put(Card.SPADE_ACE, sa);
+        cards.put(Card.DIAMOND_ACE, da);
+        cards.put(Card.HEART_ACE, ha);
     }
 
+    @UiField Image c7;
+    @UiField Image s7;
+    @UiField Image d7;
+    @UiField Image h7;
+    @UiField Image c8;
+    @UiField Image s8;
+    @UiField Image d8;
+    @UiField Image h8;
+    @UiField Image c9;
+    @UiField Image s9;
+    @UiField Image d9;
+    @UiField Image h9;
+    @UiField Image c10;
+    @UiField Image s10;
+    @UiField Image d10;
+    @UiField Image h10;
+    @UiField Image cj;
+    @UiField Image sj;
+    @UiField Image dj;
+    @UiField Image hj;
+    @UiField Image cq;
+    @UiField Image sq;
+    @UiField Image dq;
+    @UiField Image hq;
+    @UiField Image ck;
+    @UiField Image sk;
+    @UiField Image dk;
+    @UiField Image hk;
+    @UiField Image ca;
+    @UiField Image sa;
+    @UiField Image da;
+    @UiField Image ha;
 }
---
+++ src/main/java/com/preferanser/server/business/Card.java
@@ -0,0 +1,83 @@
+/*
+ * Preferanser is a program to simulate and calculate Russian Preferans Card game deals.
+ *
+ *     Copyright (C) 2013  Yuriy Lazarev <Yuriy.Lazarev@gmail.com>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see [http://www.gnu.org/licenses/].
+ */
+
+package com.preferanser.server.business;
+
+/**
+ * Playing Card
+ */
+public enum Card {
+
+    CLUB_SEVEN(Suit.CLUB, Rank.SEVEN),
+    SPADE_SEVEN(Suit.SPADE, Rank.SEVEN),
+    DIAMOND_SEVEN(Suit.DIAMOND, Rank.SEVEN),
+    HEART_SEVEN(Suit.HEART, Rank.SEVEN),
+
+    CLUB_EIGHT(Suit.CLUB, Rank.EIGHT),
+    SPADE_EIGHT(Suit.SPADE, Rank.EIGHT),
+    DIAMOND_EIGHT(Suit.DIAMOND, Rank.EIGHT),
+    HEART_EIGHT(Suit.HEART, Rank.EIGHT),
+
+    CLUB_NINE(Suit.CLUB, Rank.NINE),
+    SPADE_NINE(Suit.SPADE, Rank.NINE),
+    DIAMOND_NINE(Suit.DIAMOND, Rank.NINE),
+    HEART_NINE(Suit.HEART, Rank.NINE),
+
+    CLUB_TEN(Suit.CLUB, Rank.TEN),
+    SPADE_TEN(Suit.SPADE, Rank.TEN),
+    DIAMOND_TEN(Suit.DIAMOND, Rank.TEN),
+    HEART_TEN(Suit.HEART, Rank.TEN),
+
+    CLUB_JACK(Suit.CLUB, Rank.JACK),
+    SPADE_JACK(Suit.SPADE, Rank.JACK),
+    DIAMOND_JACK(Suit.DIAMOND, Rank.JACK),
+    HEART_JACK(Suit.HEART, Rank.JACK),
+
+    CLUB_QUEEN(Suit.CLUB, Rank.QUEEN),
+    SPADE_QUEEN(Suit.SPADE, Rank.QUEEN),
+    DIAMOND_QUEEN(Suit.DIAMOND, Rank.QUEEN),
+    HEART_QUEEN(Suit.HEART, Rank.QUEEN),
+
+    CLUB_KING(Suit.CLUB, Rank.KING),
+    SPADE_KING(Suit.SPADE, Rank.KING),
+    DIAMOND_KING(Suit.DIAMOND, Rank.KING),
+    HEART_KING(Suit.HEART, Rank.KING),
+
+    CLUB_ACE(Suit.CLUB, Rank.ACE),
+    SPADE_ACE(Suit.SPADE, Rank.ACE),
+    DIAMOND_ACE(Suit.DIAMOND, Rank.ACE),
+    HEART_ACE(Suit.HEART, Rank.ACE);
+
+    private Suit suit;
+    private Rank rank;
+
+    private Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+}
